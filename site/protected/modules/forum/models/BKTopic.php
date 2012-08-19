dfb3bab7916142334d82bc3fa8d6361674a4c49b
f0t0n <t.34.oxygen@gmail.com>
2012-08-19 15:11:02 +0300

    Models and breadcrumbs 002

---
+++ site/protected/modules/forum/models/BKTopic.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * This is the model class for table "{{topic}}".
+ *
+ * The followings are the available columns in table '{{topic}}':
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property integer $forum_id
+ *
+ * The followings are the available model relations:
+ * @property Post[] $posts
+ * @property Forum $forum
+ */
+class BKTopic extends ForumActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return BKTopic the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+	
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return '{{topic}}';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('title, forum_id', 'required'),
+			array('forum_id', 'numerical', 'integerOnly'=>true),
+			array('title, description', 'length', 'max'=>255),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, title, description, forum_id', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'posts' => array(self::HAS_MANY, 'Post', 'topic_id'),
+			'forum' => array(self::BELONGS_TO, 'Forum', 'forum_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'title' => 'Title',
+			'description' => 'Description',
+			'forum_id' => 'Forum',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('title',$this->title,true);
+		$criteria->compare('description',$this->description,true);
+		$criteria->compare('forum_id',$this->forum_id);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
